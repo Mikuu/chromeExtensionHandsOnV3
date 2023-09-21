8a4e86fd7fd236ecf25357a3be8b27cfed90a795
ariman <biaofu@thoughtworks.com>
2023-09-21 21:26:53 +0800

    init

--- src/MyAPP.vue
+++ src/MyAPP.vue
@@ -1,15 +1,75 @@
 <template>
-  <div class="app">
-    Just do it
-  </div>
+  <v-table fixed-header height="100%" class="main">
+    <thead>
+    <tr>
+      <th class="text-left roomName text-subtitle-2">
+        Room Name
+      </th>
+      <th class="text-left machine text-subtitle-2">
+        Machine
+      </th>
+      <th class="text-left timeslots text-subtitle-2">
+        Timeslot
+      </th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr
+        v-for="room in rooms"
+        :key="room.roomName"
+    >
+      <td class="font-weight-light">{{ room.roomName }}</td>
+      <td class="font-weight-light">{{ room.machine }}</td>
+<!--      <td class="font-weight-light">{{ room.timeslots.join(', ') }}</td>-->
+      <td class="font-weight-light">{{ message }}</td>
+    </tr>
+    </tbody>
+  </v-table>
 </template>
 
 <script setup>
+import {ref} from "vue";
+
+const rooms = []
+const message = ref("");
+
+for (let i=0; i<100; i++) {
+  rooms.push({ roomName: `room${i}`, machine: `machine${i}`, timeslots: ['time1', 'time2', 'time3', 'time4'] });
+}
+
+(async () => {
+
+  const injectedFunction = () => {
+    console.log(`injected ${document.title}`);
+    return document.title;
+  };
+
+  const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+  const response = await chrome.scripting.executeScript({
+        target : {tabId : tab.id},
+        func : injectedFunction,
+  });
+
+  console.log(`popup: ${response[0].result}`);
+  message.value = response[0].result;
+
+})();
+
+
 </script>
 
 <style scoped>
-.app {
-  height: 720px;
-  width: 1280px;
+.main {
+  width: 800px;
+  height: 600px;
+}
+.roomName {
+  width: 15%;
+}
+.machine {
+  width: 15%;
+}
+.timeslots {
+  width: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
-import './assets/main.css'
-
+import 'vuetify/styles'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-// import App from './App.vue'
 import MyApp from './MyAPP.vue'
 import router from './router'
 
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 const app = createApp(MyApp)
 
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
 
